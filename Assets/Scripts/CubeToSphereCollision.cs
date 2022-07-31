e3ca1decfc5f557a41ba6b39274143d7c29415b2
Spork <zoiss1999@gmail.com>
2022-08-01 00:08:17 +1000

    Finished W1 Tasks
    Added box collider file and fresh unity scene for the workshop questions.

---
+++ Assets/Scripts/CubeToSphereCollision.cs
@@ -0,0 +1,28 @@
+﻿// COMP30019 - Graphics and Interaction
+// (c) University of Melbourne, 2022
+
+using UnityEngine;
+
+public class CubeToSphereCollision : MonoBehaviour
+{
+    private BoxCollider b_collider;
+    private SphereCollider s_collider;
+    private MeshFilter curr_mesh;
+
+    [SerializeField] private Mesh sphereModel;
+
+	private void Start()
+	{
+        b_collider = GetComponent<BoxCollider>();
+        s_collider = GetComponent<SphereCollider>();
+        curr_mesh = GetComponent<MeshFilter>();
+    }
+
+
+	private void OnCollisionEnter()
+    {
+        b_collider.enabled = !b_collider.enabled;
+        curr_mesh.mesh = sphereModel;
+        s_collider.enabled = !s_collider.enabled;
+    }
+}
